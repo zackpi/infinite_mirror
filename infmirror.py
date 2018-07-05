378d135ea2e2e6465280a66530e2156ec6843d04
Zachary Pitcher <zackpi@mit.edu>
2018-07-05 15:11:47 -0400

    employing object oriented stuff from tkinter and trying to get the most out of them instead of writing my own solutions. hopefully I'll get a cleaner result this way

--- infmirror.py
+++ infmirror.py
@@ -10,15 +10,20 @@ class Interact:
         self.imfile = None
         self.image = None
         self.draw = None
+        self.default_background()
+        
+        self.xoff, yoff = 0,0
         self.disp = None
         self.dirty = True
         
+        self.width = 1
+        self.height = 1
         self.root = tk.Tk()
         self.root.title("Recursive Image Generator")
+        self.root.minsize(120, 100)
         
         self.menu = tk.Menu(self.root)
         self.root.config(menu=self.menu)
-        self.root.bind("<Configure>", self.on_resize)
 
         self.filemenu = tk.Menu(self.menu, tearoff=0)
         self.filemenu.add_command(label="Open", command=self.load)
@@ -31,27 +36,26 @@ class Interact:
         self.helpmenu.add_command(label="About", command=self.root.quit)
         self.menu.add_cascade(label="Help", menu=self.helpmenu)
         
-        self.canvas = tk.Canvas(self.root, width=640, height=480)
+        self.canvas = tk.Canvas(self.root, bg="#000", width=640, height=480)
         self.canvas.bind("<Button-1>", self.on_mouseclick)
         self.canvas.bind("<B1-Motion>", self.on_mousemove)
         self.canvas.bind("<ButtonRelease-1>", self.on_mousedrop)
+        self.canvas.bind("<Configure>", self.on_resize)
         self.canvas.pack(fill="both", expand=True)
         self.canvas.focus_set()
+        
+        self.window = [(50,40),(70,40),(70,60),(50,60)]
         
         self.redraw_timer = self.root.after(100, self.redraw)
         self.root.mainloop()
     
     def make_recursive_image(self, dst):
         if self.image is None:
-            defaulttext = "Use File>Open to select an image to modify"
-            (width, height), thick = cv2.getTextSize(defaulttext, cv2.FONT_HERSHEY_SIMPLEX, .5, 1)
-            self.draw = cv2.putText(np.full((480,640), (0,0,0), dtype=(np.uint8,3)),
-                                    defaulttext, (320-width//2,240-height//2), 
-                                    cv2.FONT_HERSHEY_SIMPLEX, .5, (100,100,100))
+            self.default_background()
             return
         self.draw = self.image.copy()
         
-        r,c,_ = self.draw.shape
+        r,c = self.draw.shape[:2]
         src = np.array([(0,0),(c,0),(c,r),(0,r)])
         dst = np.array(dst)
         hom, _ = cv2.findHomography(src, dst)
@@ -62,27 +66,41 @@ class Interact:
             self.draw = cv2.bitwise_or(cutout, warp)
     
     def redraw(self):
+        self.canvas.unbind("<Configure>")
+        r,c = self.draw.shape[:2]
+        h,w = self.height, self.width
+        wide = c/r > w/h
+        xscl = c*h/r if not wide else w
+        yscl = r*w/c if wide else h
+        xoff = (w-xscl)/2 if not wide else 0
+        yoff = (h-yscl)/2 if wide else 0
+        
         if self.dirty:
             self.dirty = False
             self.make_recursive_image([(40,5),(380,40),(400,270),(60,280)])
             
-            center = np.zeros((self.height+1, self.width), dtype=(np.uint8,3))
-            h,w = self.draw.shape[:2]
-            narrow = w/h < self.width/self.height
-            xoff = (self.width-w)//2 if narrow else 0
-            yoff = (self.height-h)//2 if not narrow else 0
-            scl = self.width/w
-            center[yoff:yoff+int(1+scl*h), xoff:xoff+int(scl*w)] = cv2.resize(self.draw, (0,0), fx=scl, fy=scl)
-            
-            img_cv2 = cv2.cvtColor(center, cv2.COLOR_BGR2RGB)
+            img_cv2 = cv2.cvtColor(self.draw, cv2.COLOR_BGR2RGB)
             img_pil = Image.fromarray(img_cv2)
             img_tk = ImageTk.PhotoImage(image=img_pil)
             self.disp = img_tk
         
-        self.canvas.delete("ALL")
-        self.canvas.create_image(self.width//2, self.height//2, image=self.disp)
+        self.canvas.delete("all")
+        self.canvas.create_image(xoff, yoff, anchor=tk.NW, image=self.disp)
+        self.canvas.create_polygon(*self.window[0], *self.window[1],
+                                    *self.window[2], *self.window[3], 
+                                    fill="#1c4", outline="#3e7", activefill="#5fa")
         self.canvas.image = self.disp
-        self.redraw_timer = self.root.after(100, self.redraw)     
+        self.canvas.addtag_all("all")
+        
+        self.canvas.bind("<Configure>", self.on_resize)
+        self.redraw_timer = self.root.after(100,self.redraw)     
+    
+    def default_background(self):
+        defaulttext = "Use File>Open to select an image to modify"
+        (width, height), thick = cv2.getTextSize(defaulttext, cv2.FONT_HERSHEY_SIMPLEX, .5, 1)
+        self.draw = cv2.putText(np.full((482,642), (0,0,0), dtype=(np.uint8,3)),
+                                defaulttext, (320-width//2,240-height//2), 
+                                cv2.FONT_HERSHEY_SIMPLEX, .5, (100,100,100))
     
     def save(self):
         self.root.after_cancel(self.redraw_timer)
@@ -102,7 +120,6 @@ class Interact:
     def load_image(self):
         self.image = cv2.imread(self.imfile)
         h,w = self.image.shape[:2]
-        self.root.minsize(w, h)
         self.dirty = True
     
     def on_mouseclick(self, event):
@@ -117,8 +134,10 @@ class Interact:
         self.clicked = False
     
     def on_resize(self, event):
+        self.canvas.scale("all",0,0,event.width/self.width,event.height/self.height)
         self.width = event.width
         self.height = event.height
+        self.dirty = True
 
 
 if __name__=="__main__":
