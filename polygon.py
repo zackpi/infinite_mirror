0beb51edc02ea3348d496e96c2e2a5f8794e5ea0
Zachary Pitcher <zackpi@mit.edu>
2018-07-08 09:28:39 -0400

    implemented hermite spline interpolation. Polygon class mostly complete.

--- polygon.py
+++ polygon.py
@@ -1,5 +1,3 @@
-from matplotlib import pyplot as plt
-
 class Polygon:
     def __init__(self, pts=None):
         if pts is None:
@@ -41,22 +39,28 @@ class Polygon:
                 min_i = i
         return i if d < sys.maxsize else None
     
-    NUM_INTERP_SEGS = 10
-    def render(self, corner="sharp"):        
+    NUM_INTERP_SEGS = 20
+    def render(self, corner="sharp", flat=False):        
         if corner == "sharp":
-            return Polygon.flatten(self.xpts, self.ypts)
+            return [self.xpts, self.ypts] if not flat else Polygon.flatten(self.xpts, self.ypts)
         
         elif corner == "bezier":
-            pts = []
+            x, y = [], []
             numsegs = (len(self.xpts)+1)*Polygon.NUM_INTERP_SEGS
             for segment in range(numsegs+1):
                 t = segment/numsegs
-                pts.append(Polygon.bezier(self.xpts, t))    # add an xvalue
-                pts.append(Polygon.bezier(self.ypts, t))    # and its yvalue
-            return pts
+                x.append(Polygon.bezier(self.xpts, t))
+                y.append(Polygon.bezier(self.ypts, t))
+            return [x,y] if not flat else Polygon.flatten(x,y)
         
         elif corner == "hermite":
-            return
+            x, y = [], []
+            for k in range(len(self.xpts)-1):
+                for segment in range(Polygon.NUM_INTERP_SEGS+(k==len(self.xpts)-2)):
+                    t_k = segment / Polygon.NUM_INTERP_SEGS
+                    x.append(Polygon.hermite(self.xpts, t_k, k))
+                    y.append(Polygon.hermite(self.ypts, t_k, k))
+            return [x,y] if not flat else Polygon.flatten(x,y)
         
         raise ValueError("Invalid corner type: %s" % corner)
     
@@ -65,14 +69,14 @@ class Polygon:
         return [j for i in zip(x, y) for j in i]
 
 
-    choose_mem = {}
+    CHOOSE_MEM = {}
     @staticmethod
     def choose(n,k):
-        if (n,k) in Polygon.choose_mem:
-            return Polygon.choose_mem[(n,k)]
+        if (n,k) in Polygon.CHOOSE_MEM:
+            return Polygon.CHOOSE_MEM[(n,k)]
         if 0 < k <= n:
             f = Polygon.choose(n-1, k-1) + Polygon.choose(n-1,k)
-            Polygon.choose_mem[(n,k)] = f
+            Polygon.CHOOSE_MEM[(n,k)] = f
             return f
         return int(not k)
     
@@ -84,38 +88,33 @@ class Polygon:
         bezval = sum([binom*bern*pt for binom, bern, pt in zip(binomial, bernstein, ctrl_pts)])
         return bezval
     
-    h_00 = lambda t: (1+2*t)*(1-t)**2
-    h_01 = lambda t: t*(1-t)**2
-    h_10 = lambda t: (3-2*t)*t**2
-    h_11 = lambda t: (t-1)*t**2
+    HERM_00 = lambda t: (1+2*t)*(1-t)**2
+    HERM_01 = lambda t: (3-2*t)*t**2
+    HERM_10 = lambda t: t*(1-t)**2
+    HERM_11 = lambda t: (t-1)*t**2
     @staticmethod
-    def hermite(p, x, k):
-        return
+    def hermite(p, t_k, k):
+        h_00 = Polygon.HERM_00(t_k)   # solve hermite basis functions
+        h_01 = Polygon.HERM_01(t_k)   # for third-degree interpolation
+        h_10 = Polygon.HERM_10(t_k)
+        h_11 = Polygon.HERM_11(t_k)
+        
+        m_k0 = (p[k+1]-p[max(0, k-1)])/2    # use finite difference method for tangents
+        m_k1 = (p[min(len(p)-1, k+2)]-p[k])/2
+        
+        hermval = h_00*p[k] + h_10*m_k0 + \
+                h_01*p[k+1] + h_11*m_k1
+        return hermval
 
 
 if __name__=="__main__":
-    p = Polygon([(1,1), (4,5), (7,6), (10,2)])
+    from matplotlib import pyplot as plt
     
-    x1, y1 = [], []
-    for i, val in enumerate(p.render(corner="bezier")):
-        if i % 2 == 0:
-            x1.append(val)
-        else:
-            y1.append(val)
+    p = Polygon([(1,1), (2,5), (7,2), (12,3), (15, 7), (20, 4)])
     
-    x2, y2 = [], []
-    for i, val in enumerate(p.render(corner="bezier")):
-        if i % 2 == 0:
-            x2.append(val)
-        else:
-            y2.append(val)
-    
-    x3, y3 = [], []
-    for i, val in enumerate(p.render(corner="hermite")):
-        if i % 2 == 0:
-            x3.append(val)
-        else:
-            y3.append(val)
+    x1, y1 = p.render(corner="bezier", flat=False)
+    x2, y2 = p.render(corner="hermite", flat=False)
+    x3, y3 = p.render(corner="sharp", flat=False)
     
     plt.plot(x1, y1, color="r")
     plt.plot(x2, y2, color="g")
